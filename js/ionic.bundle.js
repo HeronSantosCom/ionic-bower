eee7705162e266c9f426af23d16f1748e79a8a54
Heron Santos <heron@illi.com.br>
2021-11-19 11:51:14 -0300

    release: v1.3.22

--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -9,7 +9,7 @@
  * Copyright 2015 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.3.21
+ * Ionic, v1.3.22
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -25,7 +25,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.3.21';
+window.ionic.version = '1.3.22';
 
 (function (ionic) {
 
@@ -55151,7 +55151,7 @@ angular.module('ui.router.state')
  * Copyright 2015 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.3.21
+ * Ionic, v1.3.22
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
